24ccfc3f91ebfe03e30386747294c33547576f93
Anas <adelanas2000@gmail.com>
2023-09-14 12:07:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Navbar, Nav, Card, Row, Col } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">My React App</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+        </Nav>
+      </Navbar>
+
+      <Container>
+        <h1 className="mt-4">Welcome to My React App</h1>
+        
+        <Row className="mt-4">
+          <Col>
+            <Card>
+              <Card.Img variant="top" src="https://via.placeholder.com/150" />
+              <Card.Body>
+                <Card.Title>Card 1</Card.Title>
+                <Card.Text>
+                  This is a sample card.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src="https://via.placeholder.com/150" />
+              <Card.Body>
+                <Card.Title>Card 2</Card.Title>
+                <Card.Text>
+                  This is another sample card.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Img variant="top" src="https://via.placeholder.com/150" />
+              <Card.Body>
+                <Card.Title>Card 3</Card.Title>
+                <Card.Text>
+                  Yet another sample card.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
